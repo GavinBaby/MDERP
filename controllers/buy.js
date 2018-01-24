334fbdbbd7e6216e7bc5af0312e120c93e2db378
382767553@qq.com <382767553@qq.com>
2018-01-24 16:18:58 +0800

    采购模块  入库和发票生成

--- controllers/buy.js
+++ controllers/buy.js
@@ -5,6 +5,7 @@ var moment = require('moment');
 var _ = require('underscore');
 var Promise = require("bluebird");
 var knex = require('../lib/common/mysqlClient').knex;
+var bookshelf = require('../lib/common/mysqlClient').bookshelf;
 var BusinessError = require('../lib/common/errors/businessError');
 var util = require('../lib/util.js');
 var cookieParser = require('cookie-parser');
@@ -20,21 +21,42 @@ module.exports = function (app) {
     app.use('/buy', router);
 
 
-    /*********************前端*********************/
-
+    /*********************入库单填写*********************/
+//{"main":{"hd_code":1},"detail":[{"pro_id":111},{"pro_id":222}]}
     router.post('/inbill',function (req, res) {
         var main = req.body.main;
         var detail = req.body.detail;
+        //货单号码，应收款记录号,入库记录单号,收货单号,入库时间,供应商id, 供应商名称,仓库id, 仓库code,仓库name,发票号码，发票日期， 制单人,制单时间
+        main= _.pick(main,'hd_code','ysk_code','in_code','get_code','in_time','supplier_id','supplier_name','ck_id','ck_code','ck_name','fp_code','fp_time','create_man','create_time');
+        main.create_time = moment(main.create_time).format('YYYY-MM-DD HH:mm:ss');
+        main.isdeleted = 0;
+        bookshelf.transaction(function (t) {
+            return knex.max('id as id').from('t_buy_inbill').then(function (data) {
+                main.id = (data[0].id || 0) + 1;
+                detail.forEach(function (e,i) {
+                    //产品id,name,条形码，规格，整数，零数，数量，税率，税率,不含税价,不含税金额,含税单价,含税金额,备注，库存,是否计入退换卡
+                    detail[i] = _.pick(detail[i], 'pro_id', 'pro_name', 'barcode', 'gg', 'zs', 'num', 'rate', 'bhsj', 'bhsje', 'hsdj', 'hsje', 'remark', 'kc', 'is_back');
+                    detail[i].main_id = main.id;
+                });
+                return t_buy_inbill.query().insert(main);
+            }).then(function (data) {
+                return t_buy_inbill_detail.query().insert(detail);
+            }).then(function (data) {
+                res.send({code: 1});
+            })
+        }).catch(function (err) {
+            console.log("!"+err.message+"!")
+            res.send({code:-1,text:err.message});
+        })
+    });
 
-        return knex.max('id as id').from('t_buy_inbill').then(function (data) {
-           main.id=data[0].id||0 + 1;
-           detail.forEach(function (e) {
-                e.id=data[0].id||0 + 1;
-            });
-            return t_buy_inbill.query().insert(main);
-        }).then(function (data) {
-            return t_buy_inbill_detail.query().insert(detail);
-        }).then(function (data) {
+
+    /*********************入库单确认*********************/
+    // {"data":{"id":[2,3],"get_code":"123123","in_time":"2018-01-01"} }
+    router.post('/inbill/confirm',function (req, res) {
+        var data =_.pick(req.body.data,'get_code','in_time');
+        var id= _.pick(req.body.data,'id').id;
+        t_buy_inbill.query().update(data).whereIn('id',id).then(function (data) {
             res.send({code:1});
         }).catch(function (err) {
             console.log("!"+err.message+"!")
@@ -44,8 +66,14 @@ module.exports = function (app) {
 
 
 
-
-
-
+    router.get('/inbill',function (req, res) {
+        var cond =_.pick(req.body.data,'in_code','ck_id','supplier_id','in_time');
+        t_buy_inbill.query().where('isdeleted',0).andWhere(cond).then(function (data) {
+            res.send({data:data});
+        }).catch(function (err) {
+            console.log("!"+err.message+"!")
+            res.send({code:-1,text:err.message});
+        })
+    });
 
 };
---
+++ controllers/fp.js
@@ -0,0 +1,63 @@
+﻿var express = require('express');
+var router = express.Router();
+var uuid = require('node-uuid');
+var moment = require('moment');
+var _ = require('underscore');
+var Promise = require("bluebird");
+var knex = require('../lib/common/mysqlClient').knex;
+var bookshelf = require('../lib/common/mysqlClient').bookshelf;
+var BusinessError = require('../lib/common/errors/businessError');
+var util = require('../lib/util.js');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+var filter = require('../lib/filter');
+var t_buy_inbill = require('../models/table').t_buy_inbill;
+var t_buy_inbill_detail = require('../models/table').t_buy_inbill_detail;
+var t_buy_invoice = require('../models/table').t_buy_invoice;
+var t_buy_invoice_detail = require('../models/table').t_buy_invoice_detail;
+var t_buy_pay = require('../models/table').t_buy_pay;
+
+module.exports = function (app) {
+    app.use('/buy', router);
+
+
+    /*********************发票填写*********************/
+//{"main":{"code":1},"detail":[{"pro_id":111},{"pro_id":222}]}
+    router.post('/fp',function (req, res) {
+        var main = req.body.main;
+        var detail = req.body.detail;
+        //发票编号,供应商id, 供应商名称,应付款金额，扣款,发票金额,,发票日期, 入库单（逗号分隔） ,是否未结账
+        main= _.pick(main,'code','supplier_id','supplier_name','total','reduce','fp_je','fp_date','remark','bills' ,'is_part','create_man','create_time');
+        main.create_time = moment(main.create_time).format('YYYY-MM-DD HH:mm:ss');
+        main.isdeleted = 0;
+        bookshelf.transaction(function (t) {
+            return knex.max('id as id').from('t_buy_invoice').then(function (data) {
+                main.id = (data[0].id || 0) + 1;
+                detail.forEach(function (e,i) {
+                    //产品id,name,收货单号， 本次开票数量，金额
+                    detail[i] = _.pick(detail[i], 'pro_id', 'pro_name', 'billno', 'num', 'amount','is_part');
+                    detail[i].main_id = main.id;
+                });
+                return t_buy_invoice.query().insert(main);
+            }).then(function (data) {
+                return t_buy_invoice_detail.query().insert(detail);
+            }).then(function (data) {
+                res.send({code: 1});
+            })
+        }).catch(function (err) {
+            console.log("!"+err.message+"!")
+            res.send({code:-1,text:err.message});
+        })
+    });
+
+    router.get('/fp',function (req, res) {
+        var cond =_.pick(req.body.data,'code', 'supplier_id','fp_date');
+        t_buy_invoice.query().where('isdeleted',0).andWhere(cond).then(function (data) {
+            res.send({data:data});
+        }).catch(function (err) {
+            console.log("!"+err.message+"!")
+            res.send({code:-1,text:err.message});
+        })
+    });
+
+};
