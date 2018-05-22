05b28ea45a1c9910530bf8c061e66016aa52d22e
382767553@qq.com <382767553@qq.com>
2018-05-22 15:45:12 +0800

    补上

---
+++ controllers/oa.js
@@ -0,0 +1,45 @@
+﻿var express = require('express');
+var router = express.Router();
+var uuid = require('node-uuid');
+var moment = require('moment');
+var _ = require('underscore');
+var Promise = require("bluebird");
+var knex = require('../lib/common/mysqlClient').knex;
+var BusinessError = require('../lib/common/errors/businessError');
+var util = require('../lib/util.js');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+var filter = require('../lib/filter');
+var t_apply_daily = require('../models/table').t_apply_daily;
+var multiparty = require('multiparty');
+module.exports = function (app) {
+    app.use('/oa', router);
+
+    router.post('/saveDaily',function(req,res,next){
+        var form = new multiparty.Form();
+        console.log(__dirname);
+        form.encoding = 'utf-8';
+        form.uploadDir = "./upload";
+        //设置单文件大小限制
+        form.maxFilesSize = 2 * 1024 * 1024;
+        //form.maxFields = 1000;  设置所以文件的大小总和
+        form.parse(req, function(err, fields, files) {
+            if(err){
+                console.log('上传失败'+err);
+                res.send({err:'上传失败'});
+                return ;
+            }
+            var data = util.getDate(fields);
+            data.file=files.file[0].path.substr('7') ;
+            data.create_time = moment().format('YYYY-MM-DD HH:mm:ss');
+            data.isdeleted = 0;
+            t_apply_daily.query().insert(data).then(function (data) {
+                res.send({code: 1});
+            });
+            // console.log(files.files[0].originalFilename);
+            // msg.info = '上传成功'
+            // msg.len = files.length;
+            // res.writeHead(200,{"Content-type":"text/html;charset=UTF-8"});
+        });
+    });
+};
---
+++ controllers/upload.js
@@ -0,0 +1,42 @@
+﻿var express = require('express');
+var router = express.Router();
+var fs = require('fs');
+var request = require('request');
+module.exports = function (app) {
+    app.use('/upload', router);
+
+    router.get('/:file', function (req, res) {
+        res.writeHead(200, {'Content-Type': 'image/jpeg'});
+        fs.readFile('./upload/'+req.params.file, function(err, file) {
+            if (err) {
+                console.log(err);
+                return;
+            }else {
+                res.write(file,'binary');
+                res.end();
+            }
+        });
+    });
+
+
+    router.get('/down/:file', function(req, res, next) {
+        // 实现文件下载
+        var fileName = req.params.file;
+        var filePath ='./upload/'+req.params.file;
+        var stats = fs.statSync(filePath);
+        if(stats.isFile()){
+            res.set({
+                'Content-Type': 'application/octet-stream',
+                'Content-Disposition': 'attachment; filename='+fileName,
+                'Content-Length': stats.size
+            });
+            fs.createReadStream(filePath).pipe(res);
+        } else {
+            res.end(404);
+        }
+    });
+
+
+
+
+};
